eb1de104e4257834a3068afea95e6e47f28a13b2
Sultan Bin <sultanbin@protonmail.com>
2026-07-13 18:38:34 +0500

    feat: refresh portfolio layout and add live .NET news feed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SultanPortfolio.Web;
+using SultanPortfolio.Web.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddScoped<NewsFeedService>();
 
 await builder.Build().RunAsync();
